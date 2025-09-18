b32b56353c0b2245e896872dd270b910581c3954
MorganHedrick <hedric11@purdue.edu>
2025-09-18 14:05:42 +0000

    Added files for Py 3 team task 3

---
+++ Py3/py2_team_3_turbine_power_4.py
@@ -0,0 +1,38 @@
+"""
+Course Number: ENGR 13300
+Semester: e.g. Fall 2025
+
+Description:
+    Replace this line with a description of your program.
+
+Assignment Information:
+    Assignment:     7.3.1 Py1 Ind Task 1
+    Team ID:        LC5 - 04 
+    Author:         Morgan, hedric11@purdue.edu
+    Date:           09/14/2025
+
+Contributors:
+    Name, login@purdue [repeat for each]
+
+    My contributor(s) helped me:
+    [ ] understand the assignment expectations without
+        telling me how they will approach it.
+    [ ] understand different ways to think about a solution
+        without helping me plan my solution.
+    [ ] think through the meaning of a specific error or
+        bug present in my code without looking at my code.
+    Note that if you helped somebody else with their code, you
+    have to list that person as a contributor here as well.
+
+Academic Integrity Statement:
+    I have not used source code obtained from any unauthorized
+    source, either modified or unmodified; nor have I provided
+    another student access to my code.  The project I am
+    submitting is my own original work.
+"""
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
